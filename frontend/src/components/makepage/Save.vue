93343fbfeab5ab037d820f66e60f821539619e9b
이지현 <dlwlgus4553@gmail.com>
2019-01-11 21:36:26 +0900

    ...

---
+++ frontend/src/components/makepage/Save.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+    </section>
+
+</template>
+
+<script>
+    export default {
+        name: 'save',
+        data() {
+            return {};
+        },
+        
+           
+        }
+    
+</script>
+
+<style>
+</style>
