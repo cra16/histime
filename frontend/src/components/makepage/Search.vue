92be478df012b189e7abef7259dbd89aae597614
unknown <21500582@handong.edu>
2019-01-17 20:13:10 +0900

    v-for modify

--- frontend/src/components/makepage/Search.vue
+++ frontend/src/components/makepage/Search.vue
@@ -9,7 +9,86 @@
         <input type="button" class='sch_filt' value="필터" v-on:click="show"/>
        </h2>
       <div v-show="showbox" class="placeholder-box" >
+      
+      
+     
+      <p>학부<p>
+          <select v-model="학부">
+              <option v-for="course_name in course_names" :key="course_name"> 
+                  {{course_name}}
+              </option>
+         </select>
+
+      
+      <p>이수구분</p>
+          <select v-model="이수구분"> 
+              <option  v-for=" gubun in gubuns " :key="gubun">
+                {{ gubun }}
+              </option>
+          </select>
+    
+      <p>교양영역</p>
+          <select v-model="교양영역">
+                <option v-for=" gyoyang in gyoyangs" :key="gyoyang">
+                {{gyoyang}}
+                </option>
+       </select>
+       
+
       <tr>
+      <p>교수님</p>
+       <span class='blue_window2'>
+       <input id=text type="text" placeholder="교수님 이름" class='input_text' name="search" onkeydown="enterSearch()"/></span> 
+      </tr>     
+
+      <tr>
+          
+      <p>학점</p>
+            <input type="button" value="0.5">
+            <input type="button" value="1">
+            <input type="button" value="2">
+            <input type="button" value="3">
+            <input type="button" value="4">
+      </tr>
+
+      
+      <p>영어비율</p>
+      <select v-model="영어비율">
+                <option v-for=" english in englishs" :key="english">
+                    {{english}}
+                </option>
+       </select>
+
+      <p>시간대</p>
+      <input type="button" value="선택창 열기">
+    
+       <button type="button">검색하기</button>
+</div>
+    </div>
+  
+</template>
+
+<script type="text/javascript">
+
+
+/*<tr>
+      <p>이수구분</p>
+       <td align ="center">
+       <select name="crs">
+       <option value="blank">전체</option>    
+       <option value="전선">전선</option>
+       <option value="전필">전필</option>
+       <option value="자선">자선</option>
+       <option value="교선">교선</option>
+       <option value="교선필">교선필</option>
+       <option value="교필">교필</option>
+       <option value="공선">공선</option>
+       </select>
+       </td>
+       </tr>
+*/
+
+/*<tr>
       <p>학부<p>
        <td align ="center">
        <select name="keyField">
@@ -32,26 +111,13 @@
        </select>
        </td>
        </tr>
-      <tr>
-      <p>이수구분</p>
-       <td align ="center">
-       <select name="crs">
-       <option value="blank">전체</option>    
-       <option value="전선">전선</option>
-       <option value="전필">전필</option>
-       <option value="자선">자선</option>
-       <option value="교선">교선</option>
-       <option value="교선필">교선필</option>
-       <option value="교필">교필</option>
-       <option value="공선">공선</option>
-       </select>
-       </td>
-       </tr>
+       */
+      /*
        <tr>
       <p>교양영역</p>
        <td align ="center">
        <select name="liberalarts">
-       <option value="blank">전체</option>
+        <option value="blank">전체</option>
        <option value="신앙">신앙3</option>
        <option value="naturalscience">자연과학</option>
        <option value="예체능">예술</option>
@@ -77,54 +143,20 @@
        <option value="">기독교신앙기초2</option>
        <option value="">특론및개별연구</option>
        <option value="">소프트웨어활용</option>
-
-       
        </select>
        </td>
-      </tr>
-      <tr>
-      <p>교수님</p>
-       <span class='blue_window2'>
-       <input id=text type="text" placeholder="교수님 이름" class='input_text' name="search" onkeydown="enterSearch()"/></span> 
-      </tr>     
-
-      <tr>
-      <p>학점</p>
-      
-      <input type="button" value="0.5">
-      <input type="button" value="1">
-      <input type="button" value="2">
-      <input type="button" value="3">
-      <input type="button" value="4">
-
-      </tr>
-
-      <tr>
-      <p>영어비율</p>
-       <td align ="center">
-       <select name="englishrate">
-       <option value="100">100%</option>
-       <option value="70">70%</option>
-       <option value="30">30%</option>
-       <option value="0">0%</option>
-       </select>
-       </td>
-      </tr>
-      <p>시간대</p>
-      <input type="button" value="선택창 열기">
-    
-       <button type="button">검색하기</button>
-</div>
-    </div>
-  
-</template>
-
-<script type="text/javascript">
-
-
+      </tr>*/
 export default {
     data(){
         return{
+            course_names:['전체','글로벌','기계제어','경영경제','공간환경','국제어문','법학부','상담복지','생명과학','언론정보','전산전자',
+            '창의융합(인문사회)','창의융합(자연과학)','창의융합(공학)','콘텐츠융합디자인','ICT창업'],
+            gubuns: ['전체','전선','전필','자선','교선','교선필','교필','공선'],
+            gyoyangs:['전체','신앙1','신앙3','영어1','영어2','한국어','제2외국어','세계관1','세계관2',
+            '인성1','인문학','사회과학','자연과학','기독교신앙기초1','기독교신앙기초2','소통','융복합','리더십및문제해결',
+            'ICT입문','프로그래밍기초','소프트웨어활용','특론및개별연구','전공기초','예술','스포츠'],
+            englishs:['100%','0%'],
+           
             showbox:false,
             search : {
                 course_name : '',
