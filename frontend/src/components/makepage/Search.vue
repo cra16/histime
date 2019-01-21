e12d1c8512630bb7c694a65e3c4a021168994d2b
unknown <21500582@handong.edu>
2019-01-21 13:10:00 +0900

    login modified

--- frontend/src/components/makepage/Search.vue
+++ frontend/src/components/makepage/Search.vue
@@ -1,43 +1,49 @@
 <template>
-    <div>
-       <h1>과목찾기</h1>
-       <h2>
+ <div>
+       <h1 id="head">과목찾기</h1>
+       <h2 > 
       <span class='blue_window'>
-        <input v-model="course_name" id=text type="text" placeholder="과목명" class='input_text' name="search"/></span> 
-        <input type="button" class='sch_filt' value="검색" v-on:click="search_by_name"/>
+        <input v-model="course_name" type="text" placeholder="과목명" class='input_text' name="search"/></span> 
+        <input type="button" class='sch_filt' value="검색" v-on:click="search_byname"/>
       
         <input type="button" class='sch_filt' value="필터" v-on:click="show"/>
        </h2>
-<div class="contents" >
-    <div class="content" v-show="!showbox" v-for="(course, key) in search">
-            
-                <div class="section1">
-                    <p>{{course.name}}</p>
-                    <p>{{`[${course.code}]`}}</p>
-                </div>
-                <div class="section2">
-                    <p>{{course.time}}</p>
-                    <p>{{course.credit}}학점</p>
-                    <p>{{course.gubun}}</p>
-                </div>
-                <div class="section3">
-                    <p>{{course.professor}}</p>
-                    <p>영어 {{course.english}}</p>
-                </div>
-                <div  class="section4">
-                    <button id="add" v-on:click="(event) => { add_to_fav(key) }"></button>
-                    <br/>
-                    <button id="add" v-on:click="(event) => { add_to_tt(key) }"></button>
-                </div>
-                <hr />
-            </div>
-        
-    </div> 
 
+  <div class='contents'>
+        <div v-show="!showbox" v-for="(course, key) in search" >
+       
+            <div class="content" >
+                        <div class="section1">
+                            <p>{{course.name}}</p>
+                            <p>{{`[${course.code}]`}}</p>
+                        </div>
 
+                        <div class="section2">
+                            <p>{{course.time}}</p>
+                            <p>{{course.credit}}학점</p>
+                            <p>{{course.gubun}}</p>
+                        </div>
+
+                        <div class="section3">
+                            <p>{{course.professor}}</p>
+                            <p>영어 {{course.english}}</p>
+                        </div>
+
+                        <div class="section4">
+                            <button id="delete" v-on:click="del()"></button>
+                            <br/>
+                            <button id="add" v-on:click="add()"></button>
+                        </div>
+
+                        <hr/>
+             </div>
+      </div>
+    </div>
+     
+      
      
       <div v-show="showbox" class="placeholder-box" >
-            <p>학부
+            <p>학부 &emsp;&ensp;&nbsp;
                 <select v-model="filter.hakbu">
                     <option v-for="course_name in course_names" :key="course_name"> 
                         {{course_name}}
@@ -45,37 +51,32 @@
                 </select>
             <p>
             
-            <p>이수구분
+            <p>이수구분 
                 <select v-model="filter.gubun"> 
                     <option  v-for=" gubun in gubuns " :key="gubun">
                         {{ gubun }}
                     </option>
                 </select>
             
-                교양영역
+                &ensp;&nbsp;교양영역
                 <select v-model="filter.gyoyang">
                         <option v-for=" gyoyang in gyoyangs" :key="gyoyang">
                         {{gyoyang}}
                         </option>
             </select>
             </p>
-            
+        
+            <p>교수님 &ensp;&nbsp;
+            <input v-model="filter.professor" type="text" placeholder="교수님 이름"/>
+            </p>     
 
-            <tr>
-            <p>교수님</p>
-            <span class='blue_window2'>
-            <input v-model="filter.professor" id=text type="text" placeholder="교수님 이름" class='input_text' name="search" onkeydown="enterSearch()"/></span> 
-            </tr>     
-
-            <tr>
-                
-            <p>학점</p>
-                    <input type="button" value="0.5">
-                    <input type="button" value="1">
-                    <input type="button" value="2">
-                    <input type="button" value="3">
-                    <input type="button" value="4">
-            </tr>
+            <p>학점 &emsp;&ensp;&nbsp;
+                    <input type="button" class="credit" value="0.5">
+                    <input type="button" class="credit" value="1">
+                    <input type="button" class="credit" value="2">
+                    <input type="button" class="credit" value="3">
+                    <input type="button" class="credit" value="4">
+            </p>
 
             
             <p>영어비율
@@ -85,11 +86,16 @@
                         </option>
             </select>
             </p>
-            <p>시간대</p>
-            <input type="button" value="선택창 열기">
+
+            <p>시간대 &nbsp;&ensp;
+            <input type="button" class="choose" value="선택창열기">
+            </p>
             
-            <button type="button" v-on:click="search_by_filter" >검색</button>
+           <center><input type="button"  class="search" value="검색하기"></center>
        </div>
+       
+     <h1 id="foot"> </h1>   
+
     </div>
   
 </template>
@@ -126,7 +132,7 @@ export default {
         show: function(){
             this.showbox=!this.showbox;
         },
-        search_by_name: function(){
+        search_byname: function(){
             console.log(this.$session.get('student_id'));
             this.$http.post('/api/make/search/name', {
                 course_name : this.course_name,
@@ -137,120 +143,20 @@ export default {
                     
             });
             this.course_name = '';
-            
         },
-        search_by_filter: function(){
+        search_by_Filter: function(){
             this.$http.post('/api/make/search/filter', {
-                hakbu : filter.hakbu,
-                gubun : filter.gubun,
-                gyoyang : filter.gyoyang,
-                credit : filter.credit,
-                english : filter.english,
-                professor : filter.professor,
-                time : filter.time
+                course_name : this.search.course_name,
                 }).then((response) => {
                     console.log(response.data);
                     search = response.data;
             });
-            this.filter.hakbu = '';
-            this.filter.gubun = '';
-            this.filter.gyoyang = '';
-            this.filter.credit = '';
-            this.filter.english = '';
-            this.filter.professor = '';
-            this.filter.time = '';
-            this.showbox  = !this.showbox;
-        },
-
-        add_to_fav: function(key){
-            // console.log(key);
-            console.log(this.search[key].name);
-            
-            this.$http.post('/api/make/add_fav', {
-                student_id : this.$session.get('student_id'),
-                code : this.search[key].code,
-                course_name : this.search[key].name,
-                professor : this.search[key].professor,
-                time : this.search[key].time,
-                credit : this.search[key].credit
-                }).then((response) => {
-                    console.log(response.data);
-                    search = response.data;
-            });
-        },
-        add_to_tt: function(key){
-            // console.log(key);
-            console.log(this.search[key].name);
-            var data = this.parsingTime(this.search[key]);
-
-            for(var i = 0 ; i < data.length; i++){
-                console.log( "day :" + data[i].day);
-                console.log( "start :" + data[i].start);
-                console.log( "length :" + data[i].long);
-                
-            }
-
-            this.$EventBus.$emit('courses', data);
-
-            // this.$http.post('/api/make/add_fav', {
-            //     student_id : this.$session.get('student_id'),
-            //     code : this.search[key].code,
-            //     course_name : this.search[key].name,
-            //     professor : this.search[key].professor,
-            //     time : this.search[key].time,
-            //     credit : this.search[key].credit
-            //     }).then((response) => {
-            //         console.log(response.data);
-            //         search = response.data;
-            // });
-        },    
-        parsingTime: function(course){
-            var sep_time = course.time.split( ',');
-            // for(var i = 0; i< sep_time.length; i++){
-            //     console.log(sep_time[i]);
-            // }
-            var prepared_data = [];
-            if(course.time = '')return prepared_data;
-
-            prepared_data.push({
-                        code : course.code,
-                        course_name : course.name,
-                        professor : course.professor,
-                        time : course.time,
-                        credit : course.credit,
-                        
-                        day : sep_time[0].substr(0, 3),
-                        start : sep_time[0][3],
-                        long : 1,
-                    });
-            
-            for(var i = 1; i < sep_time.length; i++){
-                if(parseInt(sep_time[i-1][3]) + 1 === parseInt(sep_time[i][3])){
-                    console.log("into comparison");
-                    var temp = prepared_data.pop();
-                    console.log(temp.long);
-                    temp.long ++;
-                    prepared_data.push(temp);
-                }
-                else{
-                    prepared_data.push({
-                        code : course.code,
-                        course_name : course.name,
-                        professor : course.professor,
-                        time : course.time,
-                        credit : course.credit,
-                        
-                        day : sep_time[i].substr(0, 3),
-                        start : sep_time[i][3],
-                        long : 1,
-                    });
-                }
-            }
-            return prepared_data;
         }
+            
     }
 }
 
 </script>
 <style  src = '../../assets/Makepage/search.less' scoped>
 </style>
+
