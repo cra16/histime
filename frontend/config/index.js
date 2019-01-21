269f346c50cdf3111cf64f9762cede9f0c08fd49
gur5381 <21500670@handong.edu>
2019-01-21 13:03:03 +0900

    data passed

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -15,7 +15,7 @@ module.exports = {
 
     proxyTable: {
   '/api': {
-      target: 'http://d24640cb.ngrok.io/api',
+      target: 'http://localhost:3000/api',
     
     changeOrigin: true,
     pathRewrite: {
