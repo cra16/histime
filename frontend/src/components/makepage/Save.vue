789a0fd1aaa785c0179765ca87b3ba8f28da98ac
YuJin-Kim <hgu140@naver.com>
2019-01-21 17:18:49 +0900

    complete display Save component

--- frontend/src/components/makepage/Save.vue
+++ frontend/src/components/makepage/Save.vue
@@ -4,21 +4,22 @@
     <h1 id="head">즐겨찾기</h1>
 <!-- 여기에 이렇게 많은 정보가 필요한가? -->
     <div class='contents'>
-        <!-- <div v-for="subject in subjects" :key ="subject.code" > -->
-            <!-- <div v-for="content in this.contents" :key="content.course_name" class="content" > -->
-                 <div v-for="n in 10" :key ='n' class="content" >
+        <!-- <div v-for="subject in subjects" :key="subject.code"> -->
+
+            <div v-for="course in this.courses" :key="course.code" class="content">
+                 <!-- <div v-for="n in 10" :key ='n' class="content" > -->
                 <div class="section1">
-                    <p>{{ content.course_name }}</p>
-                    <p>[IDH20457]</p>
+                    <p>{{ course.name }}</p>
+                    <p>{{`[${course.code}]`}}</p>
                 </div>
                 <div class="section2">
-                    <p>월,목 3교시</p>
-                    <p>3학점</p>
-                    <p>전공필수</p>
+                    <p>{{course.time}}</p>
+                    <p>{{course.credit}}학점</p>
+                    <p>{{course.gubun}}</p>
                 </div>
                 <div class="section3">
-                    <p>최희열</p>
-                    <p>영어 100%</p>
+                    <p>{{course.professor}}</p>
+                    <p>영어 {{course.english}}</p>
                 </div>
                 <div  class="section4">
                     <button id="delete" v-on:click="del()"></button>
@@ -44,17 +45,22 @@ export default{
         return{
        
             active: false,
-            contents : [],
-            content :{
+            courses : [],
+            content : {
                 course_name:"",
-            },
-            subject : {
-                name : "",
-                code : "",
-                },
-              }
+            }
+            // course : {
+            //     name : "",
+            //     code : "",
+            //     time : "",
+            //     credit : "",
+            //     gubun : "",
+            //     professor : "",
+            //     english : ""
+            // },
+        }
             
-          },
+    },
     methods :{
         //즐겨찾기 항목 하나만 삭제
         del(){
@@ -87,10 +93,10 @@ export default{
             student_id : this.$session.get('student_id')
         }).then((response) => {
             if (response.status === 200) {
-                var subjects = response.data; //name, code, time, credit, gubun, professor, english
-                console.log('즐겨찾기 List : ' + subjects.length + '개');
-                for(var i = 0; i < subjects.length; i++) {
-                    console.log('즐겨찾기 ' + (i+1) + '번 과목: ' + subjects[i].name);
+                this.courses = response.data; //name, code, time, credit, gubun, professor, english
+                console.log('즐겨찾기 List : ' + this.courses.length + '개');
+                for(var i = 0; i < this.courses.length; i++) {
+                    console.log('즐겨찾기 ' + (i+1) + '번 과목: ' + this.courses[i].name);
                 }
             }
         });
