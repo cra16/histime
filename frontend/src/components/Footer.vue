f6dfb83ff3e34fb94f469a3028fa3da6f163e104
unknown <21500582@handong.edu>
2019-01-28 21:09:47 +0900

    footer modified

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -5,13 +5,14 @@
         <div>
         <hr width="70%"/>
         </div>
-       <!-- <button id="feedback" v-on:click="feedback">피드백</button> -->
-       <a href="mailto:21500582@handong.edu">피드백</a>
-       <a href="http://github.com/dlwlgus53/histime" title="github page" id=git>github page</a>
-       <a href="http://naver.com" title="이용약관">이용약관</a>
-       <a v-on:click="individual()">개인정보처리방침</a>
+    
+        <a left:10px href="http://naver.com" title="cra">About us</a>
+        <a href="mailto:21500582@handong.edu" title="email">피드백 보내기</a>
+        <a href="http://github.com/dlwlgus53/histime" title="github page" id=git>github page</a>
       
-       <p>copyright by cra &emsp;&emsp; designed by Chung YuJin</p>
+        <a v-on:click="individual()">개인정보처리방침</a>
+      
+       <p>copyright by CRA &emsp;&emsp; Designed by Chung YuJin</p>
        <router-view></router-view>
        </div> 
     </div>
