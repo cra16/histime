f51a5e731af1b6ba3ee33cb5f6f074288835c31b
unknown <21500582@handong.edu>
2019-01-28 17:38:58 +0900

    footer feedback

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -5,15 +5,17 @@
         <div>
         <hr width="70%"/>
         </div>
-          
-       <button id="feedback" v-on:click="feedback()">피드백</button>
+       <!-- <button id="feedback" v-on:click="feedback">피드백</button> -->
+       <a href="mailto:21500582@handong.edu">피드백</a>
        <a href="http://github.com/dlwlgus53/histime" title="github page" id=git>github page</a>
        <a href="http://naver.com" title="이용약관">이용약관</a>
-       <p v-on:click="individual()">개인정보처리방침</p>
+       <a v-on:click="individual()">개인정보처리방침</a>
+      
        <p>copyright by cra &emsp;&emsp; designed by Chung YuJin</p>
        <router-view></router-view>
+       </div> 
     </div>
-</div>
+
  
 </template>
 
@@ -25,7 +27,7 @@ export default {
          window.location = 'http://localhost:8080/individual'
     },
     feedback:function(){
-        alert("@handong.edu");
+        alert("문의 사항 및 피드백은 @@@@@handong.edu로 보내주세요!");
     }
  }
 
