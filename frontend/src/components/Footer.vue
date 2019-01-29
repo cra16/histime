91d4e160562a4b7e09420a09282e239bb1acb1c4
gur5381 <21500670@handong.edu>
2019-01-30 01:20:09 +0900

    router link trial

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -5,10 +5,10 @@
         <div>
         <hr width="70%"/>
         </div>
-        <a href="aboutus" title="cra">About us</a>
+        <router-link to="/aboutus" title="cra">About us</router-link>
         <a href="mailto:21500582@handong.edu" title="email">피드백 보내기</a>
         <a href="http://github.com/dlwlgus53/histime" title="github page" id=git>github page</a>
-        <a href="individual" title="law">개인정보처리방침</a>
+        <router-link to="/individual" title="law">개인정보처리방침</router-link>
         <!-- <a v-on:click="individual()">개인정보처리방침</a>
        -->
        <p>copyright by CRA &emsp;&emsp; Designed by Chung YuJin</p>
