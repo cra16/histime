ab7325aa79fbedc38d06a4a5c970a78f946f39e9
이지현 <dlwlgus4553@gmail.com>
2019-02-03 11:20:49 +0900

    과목코드 복사기능 사용하기 쉽게 변경

--- frontend/src/components/showpage/copy.vue
+++ frontend/src/components/showpage/copy.vue
@@ -4,19 +4,18 @@
         <p>코드복사 버튼을 누르면 클립보드에 자동으로 과목코드가 저장됩니다</p>
         <table class="tbl-body">
              <tr>  
-                <th>번호</th>
-                <th>과목코드-분반</th>
+                <th>코드복사</th>
                 <th>과목명</th>
                 <th>교수님</th>
-                <th>코드복사</th>
+                <th>과목코드-분반</th>
             </tr>
             
                 <tr v-for="(course, key) in this.list" :key="key">
-                    <td id="num">{{ key + 1 }}</td>
-                    <td id="code">{{course.code}}</td>
+                    <td id="btn"><button @click="copy_code(key)">코드 복사</button> </td>
                     <td id="name">{{course.course_name}}</td>
                     <td id="prof">{{course.professor}}</td>
-                    <td id="btn"><button @click="copy_code(key)">코드 복사</button> </td>
+                    <td id="code">{{course.code}}</td>
+                   
                 </tr>
 
         </table>
@@ -45,14 +44,13 @@ export default {
                 }
             });
         },
-        parsing_code(code) {
+         parsing_code(code) {//과목코드-분반 의 형태로 되어있는 데이터를 분리, 과목코드만 보여줌
             var parsing_array = code.split("-");
-
             return parsing_array[0];
         },
-        copy_code(key) {
+       
+        copy_code(key) {//과목 코드 클립보드에 복사
             var parsed_code = this.parsing_code(this.list[key].code);
-
             this.$copyText(parsed_code);
         }
     }
