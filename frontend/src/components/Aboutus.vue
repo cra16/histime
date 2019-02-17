eb7d129ddbda313a073171e7269b7c9a97817798
gur5381 <21500670@handong.edu>
2019-02-17 22:58:38 +0900

    change issues

--- frontend/src/components/Aboutus.vue
+++ frontend/src/components/Aboutus.vue
@@ -12,53 +12,53 @@
                     &emsp;이름: 정진혁<br><br>
                     &emsp;학번: 21500670&emsp;
                     &emsp;학부: 전산전자공학부 <br><br>
-                    &emsp;E-mail:21500670@handong.edu<br><br>
-                    &emsp;Blog:https://blog.naver.com/gur5381<br>
-                    &emsp;&emsp;&emsp;&ensp;https://github.com/zoomKoding/<br><br>
+                    &emsp;E-mail: 21500670@handong.edu<br><br>
+                    &emsp;Blog: <a href="https://zoomkoding.github.io" target="_blank">https://zoomkoding.github.io</a><br><br>
                     </div>
                 </div>
-            
+
+                 <br>
                     
              
                 <div style="display:block;text-align:left" class="d1">
-                        <div style="display:block;text-align:left" class="p1">
-                        <img align="left" src="../image/icon.png" width="200" height="230" id="i1">
-                        <br>
-                        &emsp;이름: 김유진<br><br>
-                        &emsp;학번: 21700140&emsp;
-                        &emsp;학부: 전산전자공학부 <br><br>
-                        &emsp;E-mail:21700140@handong.edu<br><br>
-                        &emsp;Blog:https://github.com/zoomKoding/<br><br>
-                        </div>
+                    <div style="display:block;text-align:left" class="p1">
+                    <img align="left" src="../image/icon.png" width="200" height="230" id="i1">
+                    <br>
+                    &emsp;이름: 김유진<br><br>
+                    &emsp;학번: 21700140&emsp;
+                    &emsp;학부: 전산전자공학부 <br><br>
+                    &emsp;E-mail:21700140@handong.edu<br><br>
+                    &emsp;Blog:<link href='https://zoomkoding.github.io'>https://zoomkoding.github.io<br><br>
                     </div>
-                
+                </div>
+            
                         <br>
             
-                    <div style="display:block;text-align:left" class="d1">
-                            <div style="display:block;text-align:left" class="p1">
-                            <img align="left" src="../image/ji2.png" width="200" height="230" id="i1" >
-                           <br>
-                            &emsp;이름: 이지현<br><br>
-                            &emsp;학번: 21700583&emsp;
-                            &emsp;학부: 전산전자공학부 <br><br>
-                            &emsp;E-mail:dlwlgus4553@gmail.com<br><br>
-                            &emsp;Blog:https://github.com/dlwlgus53<br><br>
-                            </div>
+                <div style="display:block;text-align:left" class="d1">
+                        <div style="display:block;text-align:left" class="p1">
+                        <img align="left" src="../image/ji2.png" width="200" height="230" id="i1" >
+                        <br>
+                        &emsp;이름: 이지현<br><br>
+                        &emsp;학번: 21700583&emsp;
+                        &emsp;학부: 전산전자공학부 <br><br>
+                        &emsp;E-mail:dlwlgus4553@gmail.com<br><br>
+                        &emsp;Blog:https://github.com/dlwlgus53<br><br>
                         </div>
-                    <br>            
-                    <div style="display:block;text-align:left" class="d1">
-                            <div style="display:block;text-align:left" class="p1">
-                            <img align="left" src="../image/hyeokin.png" width="200" height="230" id="i1">
-                            <br>
-                            &emsp;이름: 이혁인<br><br>
-                            &emsp;학번: 21500582&emsp;
-                            &emsp;학부: 전산전자공학부 <br><br>
-                            &emsp;E-mail:21500582@handong.edu<br><br>
-                            &emsp;Blog:https://github.com/HyeokIn<br><br>
-                            </div>
+                    </div>
+                <br>            
+                <div style="display:block;text-align:left" class="d1">
+                        <div style="display:block;text-align:left" class="p1">
+                        <img align="left" src="../image/hyeokin.png" width="200" height="230" id="i1">
+                        <br>
+                        &emsp;이름: 이혁인<br><br>
+                        &emsp;학번: 21500582&emsp;
+                        &emsp;학부: 전산전자공학부 <br><br>
+                        &emsp;E-mail:21500582@handong.edu<br><br>
+                        &emsp;Blog:https://github.com/HyeokIn<br><br>
                         </div>
-                            <br>
-                            <br>
+                    </div>
+                        <br>
+                        <br>
            
             <hr width="70%"/>
             <p id="category">designer</p>
--- frontend/src/components/makepage/Search.vue
+++ frontend/src/components/makepage/Search.vue
@@ -205,30 +205,8 @@ export default {
             
         },
         timebox_chosen: function(){
+            // console.log(this.checktime);
             this.searchbox=false;
-               for(var i=1;i<78; i++) {
-                    if(this.checktime[i] === undefined)continue;
-                 if(this.checktime[i]===true) {
-                    // selectbox2에서 날짜 시간 테이블 생성시 반복문 
-                    console.log('j는' + j);
-                    var day = ''; 
-                    var time = i % 11;
-                    var j = (i - time)/11;
-                    if(j === 1) day +=  '월';
-                    else if(j === 2) day += '화';
-                    else if(j === 3) day += '수';
-                    else if(j === 4) day += '목';
-                    else if(j === 5) day += '금';
-                    else if(j === 6) day += '토';
-                    console.log(`${day}${time}`);
-
-                    this.filter.time.push(`${day}${time}`);
-                    
-                         //this.checktime[i][j]===false 체크 후 done 하고 초기화
-                }
-
-            }
-          
         },
         timebox_reset:function(){
             console.log('into timebox reset');
@@ -263,7 +241,26 @@ export default {
             }
         },
         search_by_Filter: function(){
-           
+            for(var i=1;i<78; i++) {
+                    if(this.checktime[i] === undefined)continue;
+                 if(this.checktime[i]===true) {
+                    // selectbox2에서 날짜 시간 테이블 생성시 반복문 
+                    console.log('j는' + j);
+                    var day = ''; 
+                    var time = i % 11;
+                    var j = (i - time)/11;
+                    if(j === 1) day +=  '월';
+                    else if(j === 2) day += '화';
+                    else if(j === 3) day += '수';
+                    else if(j === 4) day += '목';
+                    else if(j === 5) day += '금';
+                    else if(j === 6) day += '토';
+                    console.log(`${day}${time}`);
+
+                    this.filter.time.push(`${day}${time}`);
+                    
+                }         //this.checktime[i][j]===false 체크 후 done 하고 초기화
+            }
             this.no_result = false;//'결과가 없습니다' 글씨를 잠시 지워줌
             this.search=''//검색창 초기화
             this.loading = true;//'검색중..'글자 띄워줌
