d0b329f94d737915af063012dc935f94737a7f8e
gur5381 <21500670@handong.edu>
2019-02-20 14:36:51 +0900

    google analytics enabled

--- backend/routes/make.js
+++ backend/routes/make.js
@@ -3,17 +3,37 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-// 비밀번호는 별도의 파일로 분리해서 버전관리에 포함시키지 않아야 합니다. 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'tester',
-    password : '1234',
-    database : 'histime'
-});
-connection.connect(function(err) {
-    if(err) console.log(err);
-    console.log('You are now connected...');
-});
+var db_config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'h010638847',
+    database: 'histime'
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 router.post('/', function(req, res, next) {
     // console.log(req.body.id);
